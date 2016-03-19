7cbad7de6ec684285d8b717af7b86705c1a6da22
barbagialla <some@user.com>
2016-03-19 17:23:16 +0100

    Modified templates to display posts from database.

--- blog/models.py
+++ blog/models.py
@@ -13,12 +13,12 @@ class Post(models.Model):
         blank=True, null=True)
 
 
-def publish(self):
-    self.published_date = timezone.now()
-    self.save()
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
 
 
-def __str__(self):
-    return self.title
+    def __str__(self):
+        return self.title
 
 # Create your models here.
